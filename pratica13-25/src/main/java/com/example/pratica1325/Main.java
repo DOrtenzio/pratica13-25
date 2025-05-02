566ac2f0c345f98e5e351c97de9170cd99c159a0
dortenzio.21125 <dortenzio.diego.studente@itispaleocapa.it>
2025-05-02 09:35:01 +0200

    Add Html

--- pratica13-25/src/main/java/com/example/pratica1325/Main.java
+++ pratica13-25/src/main/java/com/example/pratica1325/Main.java
@@ -5,8 +5,10 @@ import java.io.File;
 public class Main {
     public static void main(String [] args){
         //Creo il file e salvo tutto in memoria
-        FileCSV fileCSV=new FileCSV(new File("src/main/resources/com/example/pratica1325/dortenzio.csv"));
+        GestoreCSV gestoreCSV =new GestoreCSV(new File("pratica13-25/src/main/resources/com/example/pratica1325/dortenzio.csv"));
         //Aggiorno o creo il file record.csv
-        fileCSV.aggiornaFile();
+        gestoreCSV.aggiornaFile();
+        //Aggiorno html
+        gestoreCSV.aggiornaHtml();
     }
 }
